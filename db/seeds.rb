310c19e5aafee25580ab2d001b56f0bd7ecf73a6
Steve Rector <steverector27@gmail.com>
2017-10-07 12:09:48 -0700

    Adds user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+user_data = {
+  name: "Ransom",
+  participant_id: "908997180284469041"
+}
+
+new_user = User.new(user_data)
+unless new_user.save 
+  puts "NAHHHHHHHHH"
+end
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
