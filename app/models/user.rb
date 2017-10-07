cf443341927d77b7dfd3097af5e57f5bf2bd740f
Steve Rector <steverector27@gmail.com>
2017-10-07 14:45:59 -0700

    Adds ability to create accounts for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,20 @@ class User < ApplicationRecord
   has_many :accounts
 
   def create_accounts
-    p HTTParty.get('https://www.google.com')
+    # call api to get account info
+
+    # create an account for each of them
+    response = HTTParty.post('http://api119525live.gateway.akana.com:80/user/accounts',
+      :body => {LegalParticipantIdentifier: self.participant_id}.to_json,
+      :headers =>{'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+    
+    response["AccessibleAccountDetailList"].each do |account_data|
+      Account.create({
+        operating_company_identifier: account_data["OperatingCompanyIdentifier"],
+        product_code: account_data["ProductCode"],
+        primary_identifier: account_data["PrimaryIdentifier"],
+        user_id: self.id 
+        })
+    end
   end
 end
