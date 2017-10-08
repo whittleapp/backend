47ad60cae55b3dd5d47338f3ef7d50bb064c213b
Steve Rector <steverector27@gmail.com>
2017-10-07 21:01:53 -0700

    Add business id to index json object

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -6,6 +6,7 @@ class BusinessesController < ActionController::API
     businesses.each do |business|
       if business.monthly_transactions(date).count > 0
         summary << {
+          id: business.id
           business: business.name,
           transactions: business.monthly_transactions(date).count,
           total_spent: business.monthly_total(date)
@@ -17,6 +18,12 @@ class BusinessesController < ActionController::API
     }
   end
 
+  def update
+    business = Business.find_by(name: params[:name])
+    if params[:ignore] 
+      business.update(ignore: params[:ignore])
+    end
 
+  end
 
 end
